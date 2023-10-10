4c3f1e074efe29452a3e9dd9458e5e857e5e5eeb
evgBe4en <crfqg2014@gmail.com>
2023-10-10 17:18:31 +0300

    add price calculator service

---
+++ backend/app/Services/PriceCalculator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+class PriceCalculator
+{
+    public function calculate($carrier, $weight)
+    {
+        $prices = $carrier->prices;
+
+        foreach($prices as $price) {
+            if($weight >= $price->min_weight && $weight <= $price->max_weight) {
+                return $price->price;
+            }
+        }
+
+        throw new \Exception('No matching price found');
+    }
+}
